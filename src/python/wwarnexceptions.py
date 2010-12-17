46a13e01c7702fa4c7634162d07c0b3a65d9185a
carze <cesar.arze@gmail.com>
2010-12-17 10:14:17 -0500

    Adding exceptions class

---
+++ src/python/wwarnexceptions.py
@@ -0,0 +1,12 @@
+
+class AgeGroupException(Exception):
+    """ 
+    A custom exception class that should be raised when an age
+    group provided is not well-formed
+    """
+    def __init__(self, value):
+        self.error_msg = value
+        
+    def __str__(self):
+        return repr(self.error_msg) 
+    
